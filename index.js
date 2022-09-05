fa2afc29484fe3a87ee0270a425a633dcfc40702
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-09-05 14:51:43 -0300

    Started Js, added querySelectors

---
+++ index.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form')
+const submitButton = document.querySelector('#submit')
+// INPUTS
+const inputName = document.querySelector('#name')
+const inputNumber = document.querySelector('#number')
+const inputMM = document.querySelector('#month')
+const inputYY = document.querySelector('#year')
+const inputCvc = document.querySelector('#cvc')
+// OUTPUTS
+const outputNumber = document.querySelector('#outNumber')
+const outputName = document.querySelector('#outName')
+const outputDate = document.querySelector('#outDate')
+const outputCvc = document.querySelector('#outCvc')
