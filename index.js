1c4de9860531f83a41f21708900e4c409b989660
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-09-05 15:20:02 -0300

    Implemented simple form event (no validation)

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ const outputNumber = document.querySelector('#outNumber')
 const outputName = document.querySelector('#outName')
 const outputDate = document.querySelector('#outDate')
 const outputCvc = document.querySelector('#outCvc')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    outputName.innerHTML = inputName.value.toUpperCase()
+    outputNumber.innerHTML = inputNumber.value.toString().match(/.{4}/g).join(' ')
+    outputDate.innerHTML = inputMM.value + "/" + inputYY.value
+    outputCvc.innerHTML = inputCvc.value
+    form.reset()
+})
