a36bed11474ae5079d5d0b9cc208b94fbe110449
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-09-06 19:48:25 -0300

    Completed 'complete' mobile page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form')
-const submitButton = document.querySelector('#submit')
+let contDiv = document.querySelector('#completed')
 // INPUTS
+const submitButton = document.querySelector('#submit')
 const inputName = document.querySelector('#name')
 const inputNumber = document.querySelector('#number')
 const inputMM = document.querySelector('#month')
@@ -13,10 +14,32 @@ const outputDate = document.querySelector('#outDate')
 const outputCvc = document.querySelector('#outCvc')
 
 form.addEventListener('submit', (e) => {
+
     e.preventDefault()
     outputName.innerHTML = inputName.value.toUpperCase()
     outputNumber.innerHTML = inputNumber.value.toString().match(/.{4}/g).join(' ')
     outputDate.innerHTML = inputMM.value + "/" + inputYY.value
     outputCvc.innerHTML = inputCvc.value
-    form.reset()
+
+    completed()
 })
+
+let completed = () => {
+
+    form.style.display = "none";
+
+    let temp = `
+        <img src="./images/icon-complete.svg" alt="Completed icon">
+        <h1>T H A N K Y O U !</h1>
+        <h3>We've added your card details</h3>
+        <button class="continue-button" onclick="renderContinue()">Continue</button>
+    `
+    contDiv.innerHTML = temp
+
+}
+
+let renderContinue = () => {
+    contDiv.style.display = "none"
+    form.reset()
+    form.style.display = "flex"
+}
