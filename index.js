f073d421414ee299b1a3090f7894abe234b99f84
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-09-27 21:08:37 -0300

    Done form values verification + error message spans

--- index.js
+++ index.js
@@ -13,16 +13,103 @@ const outputName = document.querySelector('#outName')
 const outputDate = document.querySelector('#outDate')
 const outputCvc = document.querySelector('#outCvc')
 
+// Credit card number format & validation
+function formats(ele,e){
+    if(ele.value.length<19){
+        ele.value= ele.value.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
+        return true;
+    }else{
+        return false;
+    }
+}
+function numberValidation(e){
+    e.target.value = e.target.value.replace(/[^\d ]/g,'');
+    return false;
+}
+
 form.addEventListener('submit', (e) => {
 
     e.preventDefault()
+
+    formValidation()
+
+})
+
+let formValidation = () => {
+
+    let errors = document.querySelectorAll('.errorMsg')
+    errors.forEach(e => {
+        e.remove()
+    });
+
+    let numberDiv = document.querySelector('.numberDiv')
+
+    let errorCard = (text) => {
+        let card = document.createElement('span')
+        card.classList.add('errorMsg')
+        card.innerHTML = text
+        return card
+    }
+    
+    let pass = true
+
+    let nameValidation = () => {
+        let val = inputName.value
+        let result = /^[a-zA-Z ]+$/.test(val);
+        if (!result) {
+            let error = errorCard("Wrong format.")
+            inputName.after(error)
+            pass = false
+        }
+    }
+    nameValidation()
+
+    let mmValidation = () => {
+        let val = inputMM.value
+        let result = /^(0[1-9]|1[0-2])$/.test(val);
+        if (!result) {
+            let error = errorCard("Invalid month.")
+            numberDiv.after(error)
+            pass = false
+        }
+    }
+    mmValidation()
+
+    let yyValidation = () => {
+        let val = inputYY.value
+        let result = /^[2-9][2-9]$/.test(val);
+        if (!result) {
+            let error = errorCard("Invalid year.")
+            numberDiv.after(error)
+            pass = false
+        }
+    }
+    yyValidation()
+
+    let cvcValidation = () => {
+        let val = inputCvc.value
+        let result = /^\d\d\d$/.test(val);
+        if (!result) {
+            let error = errorCard("Invalid CVC.")
+            numberDiv.after(error)
+            pass = false
+        }
+    }
+    cvcValidation()
+
+    if (pass) { valid() }
+
+}
+
+let valid = () => {
+
     outputName.innerHTML = inputName.value.toUpperCase()
-    outputNumber.innerHTML = inputNumber.value.toString().match(/.{4}/g).join(' ')
+    outputNumber.innerHTML = inputNumber.value
     outputDate.innerHTML = inputMM.value + "/" + inputYY.value
     outputCvc.innerHTML = inputCvc.value
 
     completed()
-})
+}
 
 let completed = () => {
 
